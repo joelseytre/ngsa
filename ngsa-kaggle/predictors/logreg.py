56b77c2bedd827365b694d305ea476c322b24012
adibbaziz <adib@Wheatley.local>
2018-04-01 18:56:34 +0200

    add logistic regression

---
+++ ngsa-kaggle/predictors/logreg.py
@@ -0,0 +1,17 @@
+from predictors.NGSApredictor import NGSApredictor
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import f1_score
+
+
+class LogReg(NGSApredictor):
+    def run(self, pen, c):
+        model = LogisticRegression(penalty=pen, C = c)
+
+        model.fit(self.features['train_data'], self.train_labels)
+        self.fscore_t = f1_score(self.train_labels, model.predict(self.features['train_data']))
+        self.fscore_v = f1_score(self.valid_labels, model.predict(self.features['valid_data']))
+        print("Logistic Regression model: F1 score - Training %.3f - Validation %.3f" % (self.fscore_t, self.fscore_v))
+
+        if not self.settings['development_mode']:
+            predictions = list(model.predict(self.features['testing']))
+            self.prediction = zip(range(len(self.testing_set)), predictions)
